b260889bf84873693014a2847f47eef59356ae4f
dunnogg <119856506+dunnogg@users.noreply.github.com>
2023-12-07 13:40:56 +0300

    Update bot.ts

--- src/bot.ts
+++ src/bot.ts
@@ -2,9 +2,10 @@ import { Bot } from "grammy";
 import { type Chat } from "grammy/out/types";
 import { isEmpty } from "lodash";
 import fs from 'node:fs';
+import {clearInterval} from "timers";
 
 // Create a bot using the Telegram token
-const bot = new Bot('TOKEN');
+const bot = new Bot('6429183919:AAE3gBhKoQyD1TeDPoTxY8FgxX7NqVf5THA');
 const replyWithIntro = (ctx: any) =>
     ctx.reply(introductionMessage, {
         parse_mode: "HTML",
@@ -44,11 +45,14 @@ bot.command("statistic",(ctx) => {
         ctx.api.sendMessage(chat, `chatid неверный или отсутствует`)
     }
     else{
-        ctx.api.getChatMemberCount(chatid).then((response) => ctx.reply(`${response}`))}});
+        ctx.api.getChatMemberCount(chatid).then((response) => ctx.reply(`${response}`))}
+        });
 
-
-
-bot.command("start",(ctx) => { hours = Number(ctx.match); console.log(hour * hours)
+let timer: NodeJS.Timer;
+let StartChecker: Boolean = false;
+bot.command("start",(ctx) => { 
+    hours = Number(ctx.match);
+    console.log(hour * hours);
     if (hour * hours < 5000)
     {
         ctx.reply(`Слишком маленький перерыв между сообщениями`);
@@ -57,18 +61,30 @@ bot.command("start",(ctx) => { hours = Number(ctx.match); console.log(hour * hou
     {
         ctx.reply(`Слишком большой перерыв между сообщениями`);
     }
-    else
+    else if (isEmpty(timer))
     {
+        if (StartChecker == false)
+        {
+            StartChecker = true;
+        }
+        else
+        {
+            chat = NaN;
+        }
         let timer = setInterval(() => {
             if (isNaN(chat) || privatechannel) {
-                
-            } else {
+                clearInterval(timer)
+                return;
+            } 
+            else
+            {
+                console.log(timer);
                 const Count = bot.api.getChatMemberCount(chatid).then((response) => {
                     fs.appendFileSync("base.txt", (String(response)+' '));
                     const file = (fs.readFileSync("base.txt").toString('utf-8')).split(' ');
                     const a = Number(file[file.length - 2]);
                     const b = Number(file[file.length - 3]);
-                    
+                    console.log(timer)
                     if (a > b)
                     {
                         const dif = ((a-b)/a) * 100
@@ -91,7 +107,7 @@ bot.command("start",(ctx) => { hours = Number(ctx.match); console.log(hour * hou
                     }});
             }
             }, hour * hours);
-        timer.refresh();}});
+        }});
 
 
 bot.on("message", replyWithIntro);
